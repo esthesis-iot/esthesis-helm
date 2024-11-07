aec80d5ca2a72f868809ac5eebef62bc716c9f4d
Nassos Michas <nassosmichas@me.com>
2024-11-07 13:54:53 +0200

    chore: integrate EDGE Helm into the main repo

--- esthesis-edge/package.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-CHART_VERSION=$(grep '^version:' Chart.yaml | awk '{print $2}' | tr -d '"')
-APP_VERSION=$(grep '^appVersion:' Chart.yaml | awk '{print $2}' | tr -d '"')
-echo "CHART_VERSION: $CHART_VERSION"
-echo "APP_VERSION: $APP_VERSION"
-
-# Package the chart.
-helm package .
